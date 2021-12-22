70d7c6b07c0b1bbb4a6be2df49e3349eb3c89d3b
FeralPug <72169728+FeralPug@users.noreply.github.com>
2021-12-21 19:29:46 -0500

    Update TableOfContentsController.js

--- Scripts/TableOfContentsController.js
+++ Scripts/TableOfContentsController.js
@@ -1,7 +1,22 @@
 //this makes the jumps slightly offset so that the nav bar doesn't cover the section header
-window.addEventListener("hashchange", function () {
-    window.scrollTo(window.scrollX, window.scrollY - 100);
-});
+//window.addEventListener("hashchange", function () {
+    //window.scrollTo(window.scrollX, window.scrollY - 100);
+//});
+
+//this is better than the above because it doesn't matter if you are already at that anchor
+
+const tableOfContentsLinks = document.querySelectorAll(".AsideContainer a");
+
+for (let i = 0; i < tableOfContentsLinks.length; i++) {
+	tableOfContentsLinks[i].addEventListener("click", function () {
+		window.addEventListener("scroll", offsetScroll);
+	});
+}
+
+function offsetScroll() {
+	window.scrollTo(window.scrollX, window.scrollY - 75);
+	window.removeEventListener("scroll", offsetScroll);
+}
 
 const sections = document.querySelectorAll(".Section");
 const tableOfContents = document.querySelector(".AsideContainer ul").children;
@@ -14,35 +29,59 @@ function compareNumbers(a, b) {
 	return a - b;
 }
 
-
+let indexInView = -1;
 
 let callback = (entries) => {
 	let indices = [];
 	entries.forEach(entry => {
 		if (entry.isIntersecting) {
-			console.log(entry.intersectionRatio);
 			for (let k = 0; k < tableOfContents.length; k++) {
 				var id = tableOfContents[k].firstElementChild.getAttribute("href");
 				if (id.length > 0) {
 					if (id.substring(1) === entry.target.getAttribute("id")) {
-						//tableOfContents[k].style.color = "green";
 						indices.push(k);
+						indexInView = k;
 					}
-					else {
-						//tableOfContents[k].style.color = "blue";
-
-					}
-					tableOfContents[k].style.color = "blue";
 				}
 			}
-        }
+		}	
 	})
+
+	for (let k = 0; k < tableOfContents.length; k++) {
+		if (k != indexInView) {
+			tableOfContents[k].style.color = "blue";
+        }
+	}
 
 	if (indices.length > 0) {
 		indices = indices.sort(compareNumbers);
 
 		tableOfContents[indices[0]].style.color = "green";
 	}
+	else if(indexInView < 0) {
+		let distances = [];
+
+		for (let i = 0; i < sections.length; i++) {
+			let windowPos = window.pageYOffset;
+			let sectionPos = sections[i].offsetTop;
+
+			distances.push(Math.abs(windowPos - sectionPos));
+		}
+
+		let index = -1;
+		let smallestDist = Number.MAX_VALUE;
+
+		for (let i = 0; i < distances.length; i++) {
+			if (distances[i] <= smallestDist) {
+				index = i;
+				smallestDist = distances[i]
+            }
+		}
+
+		if (index >= 0) {
+			tableOfContents[index].style.color = "green";
+        }
+    }
 }
 
 var observer = new IntersectionObserver(callback, options);
