3ef5957d9b064d1fe3634c7638326186300fe0fc
FeralPug <72169728+FeralPug@users.noreply.github.com>
2022-02-23 12:27:34 -0500

    Adding Scripts

---
+++ Scripts/RecentPostsManager.js
@@ -0,0 +1,42 @@
+
+var hostName = location.hostname;
+
+if (hostName !== "") {
+    var recentPosts = document.getElementById("Recent");
+
+    recentPosts.firstChild.remove();
+
+    var aside = document.createElement("aside");
+
+    recentPosts.appendChild(aside);
+
+    var header = document.createElement("h2");
+
+    header.textContent = "Recent Posts";
+
+    aside.appendChild(header);
+
+    var list = document.createElement("ul");
+
+    aside.appendChild(list);
+
+    //Then make all of the elements 1 by 1
+    //first li
+
+    var entry = document.createElement("li");
+
+    var link = document.createElement("a");
+    link.setAttribute("href", "https://feralpug.github.io/OtherPages/Code/Pages/TrajectoryPredictions/TrajectoryPrediction.html");
+    link.setAttribute("title", "Link to the shader write up");
+    entry.appendChild(link);
+
+    var pic = document.createElement("img");
+    pic.setAttribute("src", hostName + "Media/RecentPosts/TrajectoryThumb.png");
+    pic.setAttribute("alt", "Screenshot of the Trajectory Predictions");
+    link.appendChild(pic);
+
+//make the next one
+}
+
+
+
